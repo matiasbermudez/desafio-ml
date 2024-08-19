edc2f49816e375ee67f37c5f73069015cbac0eff
matiasbermudez <mabgrc@hotmail.com>
2024-08-18 21:31:09 -0300

    Fin desafio

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+function crearProductos (data, contenedor)  {
+    for (let i = 0; i < 5; i++) {
+        //CREO CONTENEDOR PRINCIPAL Y LE DOY SU CLASE PARA RELLENARLO AL FINAL
+        const liEl = document.createElement('li')
+        liEl.classList.add('contenedor__productos-li')
+
+        //CREO LA IMAGEN, LE CONFIGURO SU SRC Y LA AGREGO COMO ULTIMO HIJO
+        const imgEl = document.createElement('img')
+        imgEl.src = `${data.results[i].thumbnail}`
+        liEl.append(imgEl)
+
+        //CREO CONTENEDOR PARA LA INFORMACION Y LE DOY SU CLASE
+        const divInfoEl = document.createElement('div')
+        divInfoEl.classList.add('contenedor__informacion')
+
+        //CREO EL CONTENEDOR PARA EL H3 Y EL ESTADO DEL PRODUCTO
+        const divInfoContEl = document.createElement('div')
+
+        //CREO EL H3 PARA EL NOMBRE DEL PRODUCTO Y LE CARGO SU TITLE CON TEXTCONTENT 
+        const tituloEl = document.createElement('h3')
+        tituloEl.textContent = `${data.results[i].title}`
+        //CREO EL P PARA EL ESTADO DEL PRODUCTO Y LE CARGO SU CONDITION CON TEXTCONTENT 
+        const condicionEl = document.createElement('p')
+        condicionEl.textContent = `${data.results[i].condition}`
+        //CREADO Y CARGADOS LOS H3 Y P LO AGREGO COMO APPENDCHILD AL CONTENEDOR DE INFORMACION
+        divInfoContEl.appendChild(tituloEl)
+        divInfoContEl.appendChild(condicionEl)
+        //AGREGO EL CONTENEDOR AL DIVINFO
+        divInfoEl.appendChild(divInfoContEl)
+        //CREO Y CARGO LA CANTIDAD DE VENDIDOS Y LOS AGREGO COMO APPENDCHILD AL DIVINFO
+        const vendidosEl = document.createElement('p')
+        vendidosEl.textContent = `Vendidos : ${data.results[i].installments.amount}`
+        divInfoEl.appendChild(vendidosEl)
+        //CARGADA Y CREADA TODA LA INFO LA AGREGO AL NUEVODIV QUE ES EL CONTENEDOR PRINCIPAL DEL PRODUCTO UNITARIO
+        liEl.appendChild(divInfoEl)
+        //CREO EL SPAN DE PRECIO Y LO AGREGO A CONTENEDOR PRINCIPAL DEL PRODUCTO UNITARIO
+        const precioEl = document.createElement('span')
+        precioEl.textContent = `$ ${data.results[i].price}`
+        liEl.appendChild(precioEl)
+        //CARGO EL CONTENEDOR UNITARIO AL CONTENEDOR GENERAL DE LA WEB
+        contenedor.appendChild(liEl)
+    }
+}
+function quitarProductos (contenedor) {
+    //MIENTRAS TENGO 1 ELEMENTO HIJO, LO ELIMINO
+    while(contenedor.firstChild){
+        contenedor.firstChild.remove()
+    }
+}
+
 function main() {
     const form = document.querySelector('#form__busqueda');
     const contenedorDeProductos = document.querySelector('.contenedor__productos')
@@ -8,48 +58,10 @@ function main() {
         fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${producto}`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data)
-                for (let i = 0; i < 5; i++) {
-                    //CREO CONTENEDOR PRINCIPAL Y LE DOY SU CLASE PARA RELLENARLO AL FINAL
-                    const nuevoDivEl = document.createElement('div')
-                    nuevoDivEl.classList.add('contenedor__productos-div')
-
-                    //CREO LA IMAGEN, LE CONFIGURO SU SRC Y LA AGREGO COMO ULTIMO HIJO
-                    const imgEl = document.createElement('img')
-                    imgEl.src = `${data.results[i].thumbnail}`
-                    nuevoDivEl.append(imgEl)
-
-                    //CREO CONTENEDOR PARA LA INFORMACION Y LE DOY SU CLASE
-                    const divInfoEl = document.createElement('div')
-                    divInfoEl.classList.add('contenedor__informacion')
-
-                    //CREO EL CONTENEDOR PARA EL H3 Y EL ESTADO DEL PRODUCTO
-                    const divInfoContEl = document.createElement('div')
-
-                    //CREO EL H3 PARA EL NOMBRE DEL PRODUCTO Y LE CARGO SU TITLE CON TEXTCONTENT 
-                    const tituloEl = document.createElement('h3')
-                    tituloEl.textContent = `${data.results[i].title}`
-                    //CREO EL P PARA EL ESTADO DEL PRODUCTO Y LE CARGO SU CONDITION CON TEXTCONTENT 
-                    const condicionEl = document.createElement('p')
-                    condicionEl.textContent = `${data.results[i].condition}`
-                    //CREADO Y CARGADOS LOS H3 Y P LO AGREGO COMO APPENDCHILD AL CONTENEDOR DE INFORMACION
-                    divInfoContEl.appendChild(tituloEl)
-                    divInfoContEl.appendChild(condicionEl)
-                    //AGREGO EL CONTENEDOR AL DIVINFO
-                    divInfoEl.appendChild(divInfoContEl)
-                    //CREO Y CARGO LA CANTIDAD DE VENDIDOS Y LOS AGREGO COMO APPENDCHILD AL DIVINFO
-                    const vendidosEl = document.createElement('p')
-                    vendidosEl.textContent = `Vendidos : ${data.results[i].installments.amount}`
-                    divInfoEl.appendChild(vendidosEl)
-                    //CARGADA Y CREADA TODA LA INFO LA AGREGO AL NUEVODIV QUE ES EL CONTENEDOR PRINCIPAL DEL PRODUCTO UNITARIO
-                    nuevoDivEl.appendChild(divInfoEl)
-                    //CREO EL SPAN DE PRECIO Y LO AGREGO A CONTENEDOR PRINCIPAL DEL PRODUCTO UNITARIO
-                    const precioEl = document.createElement('span')
-                    precioEl.textContent = `$ ${data.results[i].price}`
-                    nuevoDivEl.appendChild(precioEl)
-                    //CARGO EL CONTENEDOR UNITARIO AL CONTENEDOR GENERAL DE LA WEB
-                    contenedorDeProductos.appendChild(nuevoDivEl)
-                }
+                //QUITO PRODUCTOS
+                quitarProductos(contenedorDeProductos);
+                //AGREGO LOS 5 NUEVOS
+                crearProductos(data, contenedorDeProductos);
             })
     })
 
